8ec742a8fd82452b29ca412c778f023facb6af8e
AYUSH KUMAR <inc.ayush29@gmail.com>
2022-12-03 00:45:51 +0530

    Add files via upload

---
+++ BookList/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Razorpage.Model;
+
+namespace Razorpage.Pages.BookList
+
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet()
+        {
+            Books = await _db.Book.ToListAsync();
+        }
+
+    }
+}
